b271d38a25db13e79e8855011fc781cf275aaac0
yun5ol <yunsol2095@gmail.com>
2023-01-15 13:34:50 +0900

    initial commit

---
+++ 윤솔/hackathon2-backend/app/src/main/java/bitcamp/bootapp/dao/ViewDao.java
@@ -0,0 +1,29 @@
+package bitcamp.bootapp.dao;
+
+import java.util.Arrays;
+
+import org.springframework.stereotype.Repository;
+
+import bitcamp.bootapp.vo.Board;
+
+@Repository
+public class ViewDao {
+  // return된 viewCount 끼리 값 비교
+
+	public void main(String[] args) {
+		int[] view = new int[100];
+		
+		for(int 1; i<view.length; i++) {
+			view[i]=i
+		}
+	}
+	
+	
+	
+	
+	
+}
+
+
+
+
