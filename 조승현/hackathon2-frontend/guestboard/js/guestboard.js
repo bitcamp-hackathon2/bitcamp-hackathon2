ef3daa093148677fa986c6f46d59f7e5f0d3751e
whtndgus <whtndgus12@gmail.com>
2023-01-16 12:15:04 +0900

    .

---
+++ 조승현/hackathon2-frontend/guestboard/js/guestboard.js
@@ -0,0 +1,114 @@
+const writeGuestBoard = document.querySelector('#writeGuestBoard');
+const writeDiv = document.querySelector('#writeDiv');
+const writeCloseBtn = document.querySelector('#writeCloseBtn');
+const writeBG = document.querySelector('#writeBG');
+const writeArea = document.querySelector('#writeArea');
+const charCount = document.querySelector('#charCount');
+const writeBtn = document.querySelector('#writeBtn');
+
+
+
+writeGuestBoard.addEventListener('click', () => {
+  writeAreaCalcLen();
+  writeArea.innerText = "";
+  writeDiv.classList.remove("writeDivHide");
+  writeBtn.addEventListener('click', createGuestboard);
+})
+
+writeCloseBtn.addEventListener('click', () => {
+  writeDiv.classList.add("writeDivHide");
+})
+
+// 글쓰기 상태에서 배경 클릭시 창 닫지 않기
+// writeBG.addEventListener('click', hideWriteGuestboard);
+
+writeArea.addEventListener('keyup', writeAreaCalcLen);
+
+
+// 글쓰기 상태에서 배경 클릭시 창 닫지 않기
+function hideWriteGuestboard() {
+  const clickWriteCloseBtn = new MouseEvent('click');
+  writeCloseBtn.dispatchEvent(clickWriteCloseBtn);
+}
+
+function getElapsedTime(createdTime) {
+  const nowTime = new Date().getTime();
+  const elapsedTime = nowTime - createdTime;
+  const elapsedSecond = Math.floor(elapsedTime / 1000);
+  const elapsedMinute = Math.floor(elapsedSecond / 60);
+  const elapsedHour = Math.floor(elapsedMinute / 60);
+  const elapsedDay = Math.floor(elapsedHour / 24);
+  const elapsedMonth = Math.floor(elapsedDay / 30);
+  const elapsedYear = Math.floor(elapsedDay / 365);
+
+  if (elapsedSecond < 60) {
+    return `${elapsedSecond}초 전`;
+  } else if (elapsedMinute < 60) {
+    return `${elapsedMinute}분 전`;
+  } else if (elapsedHour < 24) {
+    return `${elapsedHour}시간 전`;
+  } else if (elapsedDay < 30) {
+    return `${elapsedDay}일 전`;
+  } else if (elapsedYear < 1) {
+    return `${elapsedMonth}개월 전`;
+  } else {
+    return `${elapsedYear}년 전`;
+  }
+}
+
+function writeAreaCalcLen() {
+  const textLen = writeArea.value.length;
+  let calcLen = 0;
+  for (let i = 0; i < textLen; i++) {
+    calcLen += writeArea.value.charCodeAt(i) >= 0 && writeArea.value.charCodeAt(i) <= 127 ? 0.5 : 1;
+  }
+  charCount.innerText = 140 - Math.ceil(calcLen);
+  if (140 - Math.ceil(calcLen) < 0) {
+    if (!charCount.classList.contains('charCountRed')) {
+      charCount.classList.add('charCountRed');
+    }
+    if (!writeBtn.classList.contains('charCountRed')) {
+      writeBtn.classList.add('charCountRed');
+    }
+    
+    // [남기기] 링크 제거
+    if (writeBtn.classList.contains('clickable')) {
+      writeBtn.classList.remove('clickable');
+      writeBtn.removeEventListener('click', createGuestboard);
+    }
+    if (writeBtn.classList.contains('clickable-green')) {
+      writeBtn.classList.remove('clickable-green');
+    }
+    
+  } else {
+    if (charCount.classList.contains('charCountRed')) {
+      charCount.classList.remove('charCountRed');
+    }
+    if (writeBtn.classList.contains('charCountRed')) {
+      writeBtn.classList.remove('charCountRed');
+    }
+    
+    // [남기기] 링크 활성화
+    if (!writeBtn.classList.contains('clickable')) {
+      writeBtn.classList.add('clickable');
+      writeBtn.addEventListener('click', createGuestboard);
+    }
+    if (!writeBtn.classList.contains('clickable-green')) {
+      writeBtn.classList.add('clickable-green');
+    }
+  }
+}
+
+function createGuestboard() {
+  hideWriteGuestboard();
+  requestPost();
+}
+
+function updateGuestboard(e, dataNo) {
+  hideWriteGuestboard();
+  requestUpdate(dataNo);
+}
+
+function removeLikeId(userId, dataNo) {
+  
+}
---
+++ 조승현/hackathon2-frontend/guestboard/js/guestboardFetch.js
@@ -0,0 +1,193 @@
+// let all = location.href.split("?")[1];
+// let name = all.split("=")[1].split(",")[0];
+// let id = all.split("=")[1].split(",")[1];
+
+
+
+    // ${(id === e.id) ? loginIdDelUpBtn : ""}
+let all = location.href.split("?")[1];
+let username = decodeURI(all.split("=")[1].split(",")[0]);
+let id  = all.split("=")[1].split(",")[1]
+
+
+fetch("http://localhost:8080/guestboard")
+.then((response) => response.json())
+.then((obj) => {
+
+  for (let i = 0; i < obj.data.length; i++) {
+      setTimeout(() => {
+        const e = obj.data[i];
+        const ul = document.querySelector('#guestbook-ul');
+        const li = document.createElement('li');
+        li.classList.add('board');
+        const liContent =
+          `<div>
+              <span class="name">${e.name}</span>
+              <span class="id">@${e.id}</span>
+              <span class="elapsedTime">${getElapsedTime(e.createdTime)}</span>
+              <span class="likeSpan">
+                <span class="likeIcon clickable ${isLiked(e.id, e.likeId)}" data-no="${e.no}"></span>
+                <span class="like"> ${e.like}</span>
+              </span>
+            </div>
+            <hr>
+            <div class="contentMain">
+              <div class="content">${e.content}</div>
+              <div class="del-up-btn">
+                ${loginIdDelUpBtn(e.no)}
+              </div>
+            </div>
+          `;
+        
+        li.innerHTML = liContent;
+        ul.appendChild(li);
+      }, -(i-obj.data.length)*500);
+    
+  };
+  
+setTimeout(() => {
+  workAfterLoad();
+}, obj.data.length*500+500);
+  })
+  .catch((err) => {
+    // alert('서버 요청 오류!');
+    console.log(err);
+  })
+// 
+
+function isLiked(userId, likeId) {
+  return likeId?.includes(userId) ? 'liked' : '';
+}
+
+function requestPost() {
+  const content = document.querySelector('#writeArea').value;
+
+  fetch('http://localhost:8080/guestboard', {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/x-www-form-urlencoded'
+    },
+    body: `id=${id}&content=${content}&name=${username}`
+  })
+    .then((response) => response.json())
+    .then((obj) => {
+      location.href = 'guestboard.html?'+all;
+    })
+    .catch((err) => {
+      alert('글쓰기 요청 오류!');
+      console.log(err);
+  })
+}
+
+function requestUpdate(dataNo) {
+  const content = document.querySelector('#writeArea').value;
+  
+  fetch('http://localhost:8080/guestboard', {
+    method: 'PUT',
+    headers: {
+      'content-type': 'application/x-www-form-urlencoded'
+    },
+    body: `no=${dataNo}&content=${content}`
+  })
+    .then((response) => response.json())
+    .then((obj) => {
+      location.href = 'guestboard.html?'+all;
+    })
+    .catch((err) => {
+      alert('수정 요청 오류!');
+      console.log(err);
+  })
+}
+
+function requestUpdateLike(userId, dataNo) {
+  
+  fetch('http://localhost:8080/guestboard', {
+    method: 'PUT',
+    headers: {
+      'content-type': 'application/x-www-form-urlencoded'
+    },
+    body: `id=${userId}&no=${dataNo}`
+  })
+    .then((response) => response.json())
+    .then((obj) => {
+      
+    })
+    .catch((err) => {
+      alert('수정 요청 오류!');
+      console.log(err);
+  })
+}
+
+function requestDelete(dataNo) {
+  fetch('http://localhost:8080/guestboard', {
+    method: 'DELETE',
+    headers: {
+      'content-type': 'application/x-www-form-urlencoded'
+    },
+    body: `no=${dataNo}`
+  })
+    .then((response) => response.json())
+    .then((obj) => {
+      location.href = 'guestboard.html?'+all;
+    })
+    .catch((err) => {
+      alert('삭제 요청 오류!');
+      console.log(err);
+  })
+}
+
+const loginIdDelUpBtn = function (GuestboardNo) {
+return  `<span id="update-btn" class="clickable clickable-green contentUpdBtn" data-no="${GuestboardNo}">[수정]</span>
+  <span id="delete-btn" class="clickable clickable-green contentDelBtn" data-no="${GuestboardNo}">[삭제]</span>`;
+}
+
+function workAfterLoad() {
+  const contentUpdBtn = document.querySelectorAll('.contentUpdBtn');
+  const contentDelBtn = document.querySelectorAll('.contentDelBtn');
+  const likeIcon = document.querySelectorAll('.likeIcon');
+  
+  // 좋아요 버튼 클릭 처리
+  likeIcon.forEach(item => item.addEventListener('click', (e) => {
+    let oldLikeCnt = e.target.parentElement.children[1].innerText;
+    let minusLikeCnt = parseInt(oldLikeCnt) - 1;
+    let plusLikeCnt = parseInt(oldLikeCnt) + 1;
+    
+    if (e.target.classList.contains('liked')) {
+      // 좋아요 누른 게시물 처리
+      e.target.classList.remove('liked');
+      e.target.parentElement.children[1].innerText = minusLikeCnt;
+    
+    } else {
+      // 좋아요 안 누른 게시물 처리
+      e.target.classList.add('liked');
+      e.target.parentElement.children[1].innerText = plusLikeCnt;
+    }
+    requestUpdateLike(id, e.target.getAttribute('data-no'));
+  }))
+
+  // 게시물 수정 버튼 클릭 처리
+  contentUpdBtn.forEach(item => item.addEventListener('click', () => {
+    console.log(14)
+    const oldContent = item.parentElement.previousElementSibling.innerText;
+    const dataNo = item.getAttribute('data-no');
+    
+    if (writeDiv.classList.contains('writeDivHide')) {
+      writeArea.innerText = oldContent;
+      writeAreaCalcLen();
+      writeDiv.classList.remove('writeDivHide');
+      writeBtn.onclick = (e) => {
+        updateGuestboard(e, dataNo)
+      }
+    }
+  }))
+
+  // 게시물 삭제 버튼 클릭 처리
+  contentDelBtn.forEach(e => e.addEventListener('click', () => {
+    if (confirm('정말 삭제 하시겠습니까?')) {
+      const dataNo = e.getAttribute('data-no');
+      requestDelete(dataNo);
+    } 
+  }))
+
+}
+
